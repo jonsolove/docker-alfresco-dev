fe9bf24f00dfea5885b2aaed58666549620c8e86
Bindu Wavell <bindu@ziaconsulting.com>
2017-02-28 09:02:41 -0700

    Add 5.2.0 and expose Postgres port

---
+++ 5.2.0.sh
@@ -0,0 +1,3 @@
+export ALF_VERSION=5.2.0
+export ALF_BIN=alfresco-content-services-installer-5.2.0-linux-x64.bin
+export ALF_URL=http://eu.dl.alfresco.com/release/enterprise/5.2/5.2.0-build-00026/${ALF_BIN}
